98f9da918cc465bc2a96cea5cbc8792c6139abb2
博爱“git config --global user.name 博爱” <sunboyan@outlook.com>
2016-06-03 14:55:45 +0800

    上传框架

---
+++ BANetManager/BANetManager/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BANetManager
+//
+//  Created by 博爱 on 16/6/3.
+//  Copyright © 2016年 博爱之家. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
